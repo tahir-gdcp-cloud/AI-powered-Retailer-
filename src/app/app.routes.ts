9bf224ff0cf10591e23c209a150d57ea750aa3c3
Tahir Mushtaq <tahir.gdcp@gmail.com>
2026-04-30 16:23:26 +0500

    routes guards added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { ChatLayoutComponent } from './components/chat-layout/chat-layout.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'chat', pathMatch: 'full' },
@@ -7,7 +8,7 @@ export const routes: Routes = [
   { path: 'chat/:id', component: ChatLayoutComponent },
   { path: 'checkout', loadComponent: () => import('./components/checkout/checkout.component').then(m => m.CheckoutComponent) },
   { path: 'login', loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent) },
-  { path: 'dashboard', loadComponent: () => import('./components/user-dashboard/user-dashboard.component').then(m => m.UserDashboardComponent) },
-  { path: 'thank-you', loadComponent: () => import('./components/thank-you/thank-you.component').then(m => m.ThankYouComponent) },
+  { path: 'dashboard', canActivate: [authGuard], loadComponent: () => import('./components/user-dashboard/user-dashboard.component').then(m => m.UserDashboardComponent) },
+  { path: 'thank-you', canActivate: [authGuard], loadComponent: () => import('./components/thank-you/thank-you.component').then(m => m.ThankYouComponent) },
   { path: '**', redirectTo: 'chat' }
 ];
